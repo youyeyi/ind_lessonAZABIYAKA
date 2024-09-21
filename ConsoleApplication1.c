238da1684ef6a28a9c26d4c366f95273279f5370
youyeyi <joseph.phelpsmode@gmail.com>
2024-09-21 10:46:32 +0300

    add .gitignore

---
+++ ConsoleApplication1.c
@@ -0,0 +1,14 @@
+﻿
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    printf("Welcome to New Programm! There is no functional yet");
+
+    return 0;
+}
+
