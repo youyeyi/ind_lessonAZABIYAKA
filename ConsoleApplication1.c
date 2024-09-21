c752131473710eaf27d28a664a969d514e8d81e1
youyeyi <joseph.phelpsmode@gmail.com>
2024-09-21 11:04:21 +0300

    added new functionality

--- ConsoleApplication1.c
+++ ConsoleApplication1.c
@@ -6,8 +6,11 @@
 
 int main()
 {
-    
-    printf("Welcome to New Application! There is no functionality yet");
+    int number;
+    printf("Welcome to New Application! There is no functional yet");
+    printf("Enter: ");
+    scanf_s("%d", &number);
+    printf("%d", number);
 
     return 0;
 }
